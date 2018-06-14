b011d8fd2071363fd32d18973deea6331b097de6
TetroVolt <raymond@sutrisno.me>
2018-06-14 14:45:38 -0500

    removed right triangle shadow artifiact

---
+++ Raymond/src/artifact_remover.m
@@ -0,0 +1,44 @@
+
+image = imread('../16px_cornercrop_glance.jpg');
+pxlen = 512 - 32;
+
+im_hq = histeq(image);
+im_bin = imbinarize(im_hq, graythresh(im_hq)*0.5);
+
+
+im_avg = zeros(pxlen);
+for i = 0:9
+    for j = 0:9
+        xmin = j * pxlen + 1;
+        xmax = xmin + pxlen - 1;
+        ymin = i * pxlen + 1;
+        ymax = ymin + pxlen - 1;
+        
+        subim = double(image(ymin:ymax, xmin:xmax)) ./ 255.0;
+        
+        im_avg = im_avg + subim / 100;
+    end
+end
+
+f = histeq(im_avg).*2;
+f = imgaussfilt(f, 9);
+f = 1 - f;
+%imshow(im);
+
+image = double(image) / 255.0;
+
+for i = 0:9
+    for j = 0:9
+        xmin = j * pxlen + 1;
+        xmax = xmin + pxlen - 1;
+        ymin = i * pxlen + 1;
+        ymax = ymin + pxlen - 1;
+        
+        filt = image(ymin:ymax, xmin:xmax) .* f;
+        image(ymin:ymax, xmin:xmax) = image(ymin:ymax, xmin:xmax) + 0.09 * filt;
+    end
+end
+
+imshow(image);
+%imshow(histeq(image));
+
