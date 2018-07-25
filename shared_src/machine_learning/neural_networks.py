9fc741d4c271be48cb94cace430adfb9e8a7632c
Chris <chriskimhki98@gmail.com>
2018-07-25 15:35:28 -0500

    NN Metrics Update

--- shared_src/machine_learning/neural_networks.py
+++ shared_src/machine_learning/neural_networks.py
@@ -10,10 +10,11 @@ Model Architecture: 3, Score: 0.939 +/- 0.100
 import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense
+from sklearn.metrics import confusion_matrix
 import pandas as pd
 import sys
 from sklearn.model_selection import StratifiedKFold # class proportions are preserved in each folder
-# from utils import *
+from utils import *
 
 def build_keras_sequential(activation='sigmoid', architecture=(12,5)):
    assert(len(architecture) >= 2)
@@ -37,20 +38,24 @@ def build_keras_sequential(activation='sigmoid', architecture=(12,5)):
 
 
 
-def train_keras_sequential(file, model,epoch_num):
+def train_keras_sequential(file,config,epoch_num):
    df = pd.read_csv(file)
-   labels = df['labels'].values
-   data = df.drop(columns=['Unnamed: 0','labels','file_names']).values 
+   data, labels = pre_process_data(file, pickled=False, feature_cols=[], label_col=-1, drop=['file_names'],
+                                   one_hot=False, shuffle=True, standard_scale=True)
+   print(data)
+   print(labels)
    kfold = StratifiedKFold(n_splits=10,random_state=1).split(data,labels)
    scores = []
+   c_matrix = np.zeros((2,2))
    for k,(train,test) in enumerate(kfold):
       y = np.eye(len(np.unique(labels)))[labels]
+      model = build_keras_sequential(architecture=config)
       model.fit(data[train],y[train],epochs=epoch_num,batch_size=10)
       score = model.evaluate(data[test],y[test],batch_size=10) # score = [test loss, test accuracy]
       scores.append(score[1])
-      #print('Fold: %2d, Class dist.: %s, Acc: %.3f' %(k+1, np.bincount(labels[train]), score[1]))
-   #print('\nCV accuracy: %.3f +/- %.3f' %(np.mean(scores), np.std(scores)))
-   return np.mean(scores), np.std(scores)
+      predictions = model.predict_classes(data[test])
+      c_matrix = c_matrix + confusion_matrix(y[test],predictions)
+   return np.mean(scores), np.std(scores), c_matrix
 
 def Main(file):
    model_configurations = [
@@ -58,16 +63,21 @@ def Main(file):
       (12, 8, 5),
       (12, 10, 7, 5),
    ]
-   epoch = [1000,1000,1000]
+   epoch = [10,10,10]
    means = []
    stds = []
+   cms = []
    for k,config in enumerate(model_configurations):
-      mean,std = train_keras_sequential(file, build_keras_sequential(architecture=config), epoch[k])
-      print(mean)
+      mean,std, cm = train_keras_sequential(file, config, epoch[k])
       means.append(mean)
       stds.append(std)
+      cms.append(cm)
    for k in range(len(model_configurations)):
-      print('Model Architecture:%2d, Score: %.3f +/- %.3f' %(k+1, means[k], stds[k]))
+      print('Model Architecture:%2d'
+            '\tScore: %.3f +/- %.3f\n\t' 
+            '\tConfusion Matrix: ' %(k+1, means[k], stds[k]))
+      print(cms[k])
+
 
 if __name__=='__main__' and len(sys.argv) > 1:
    import argparse
--- shared_src/machine_learning/support_vector_machines.py
+++ shared_src/machine_learning/support_vector_machines.py
@@ -1,3 +1,24 @@
 from sklearn import svm
+from utils import *
 
-model = svm.svc(kernel='linear',c=1,gamma=1)
+
+def build_SVM(file):
+	features, labels = pre_process_data(file, pickled=False, feature_cols=[], label_col=-1, drop=['file_names'], one_hot=False, shuffle=True):
+
+
+def Main(file)
+	build_SVM(file)
+
+if __name__=='__main__' and len(sys.argv) > 1:
+   import argparse
+   directory = sys.argv[1]
+   Main(directory)
+	
+
+# Create SVM classification object 
+model = svm.svc(kernel='linear', c=1, gamma=1) 
+# there is various option associated with it, like changing kernel, gamma and C value. Will discuss more # about it in next section.Train the model using the training sets and check score
+model.fit(X, y)
+model.score(X, y)
+#Predict Output
+predicted= model.predict(x_test)
