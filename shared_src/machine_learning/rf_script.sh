fc077f96f4f6e8840b9ac2030f3e810e9d43945f
TetroVolt <raymond@sutrisno.me>
2018-08-04 01:30:03 -0500

    fixed files in script:

--- shared_src/machine_learning/rf_script.sh
+++ shared_src/machine_learning/rf_script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-echo "data/LC_dewet_prune2_w0.png" | python gridsearch_randomforest.py -k 10 -n 50 -P --seed 1 -D 2 "data/features_dewetting_woclass0,1.csv"  1> data/pruning2_dewetting_w0.txt
-echo "data/LC_dewet_prune3_w0.png" | python gridsearch_randomforest.py -k 10 -n 50 -P --seed 1 -D 3 "data/features_dewetting_woclass0,1.csv"  1> data/pruning3_dewetting_w0.txt
-echo "data/LC_dewet_prune4_w0.png" | python gridsearch_randomforest.py -k 10 -n 50 -P --seed 1 -D 4 "data/features_dewetting_woclass0,1.csv"  1> data/pruning4_dewetting_w0.txt
-echo "data/LC_dewet_prune5_w0.png" | python gridsearch_randomforest.py -k 10 -n 50 -P --seed 1 -D 5 "data/features_dewetting_woclass0,1.csv"  1> data/pruning5_dewetting_w0.txt
-echo "data/LC_dewet_prune6_w0.png" | python gridsearch_randomforest.py -k 10 -n 50 -P --seed 1 -D 6 "data/features_dewetting_woclass0,1.csv"  1> data/pruning6_dewetting_w0.txt
-echo "data/LC_dewet_prune7_w0.png" | python gridsearch_randomforest.py -k 10 -n 50 -P --seed 1 -D 7 "data/features_dewetting_woclass0,1.csv"  1> data/pruning7_dewetting_w0.txt
-echo "data/LC_dewet_prune8_w0.png" | python gridsearch_randomforest.py -k 10 -n 50 -P --seed 1 -D 8 "data/features_dewetting_woclass0,1.csv"  1> data/pruning8_dewetting_w0.txt
+echo "data/LC_dewet_prune2_w0.png" | python gridsearch_randomforest.py -k 10 -n 50 -P --seed 1 -D 2 "data/features_dewetting_woclass1.csv"  1> data/pruning2_dewetting_w0.txt
+echo "data/LC_dewet_prune3_w0.png" | python gridsearch_randomforest.py -k 10 -n 50 -P --seed 1 -D 3 "data/features_dewetting_woclass1.csv"  1> data/pruning3_dewetting_w0.txt
+echo "data/LC_dewet_prune4_w0.png" | python gridsearch_randomforest.py -k 10 -n 50 -P --seed 1 -D 4 "data/features_dewetting_woclass1.csv"  1> data/pruning4_dewetting_w0.txt
+echo "data/LC_dewet_prune5_w0.png" | python gridsearch_randomforest.py -k 10 -n 50 -P --seed 1 -D 5 "data/features_dewetting_woclass1.csv"  1> data/pruning5_dewetting_w0.txt
+echo "data/LC_dewet_prune6_w0.png" | python gridsearch_randomforest.py -k 10 -n 50 -P --seed 1 -D 6 "data/features_dewetting_woclass1.csv"  1> data/pruning6_dewetting_w0.txt
+echo "data/LC_dewet_prune7_w0.png" | python gridsearch_randomforest.py -k 10 -n 50 -P --seed 1 -D 7 "data/features_dewetting_woclass1.csv"  1> data/pruning7_dewetting_w0.txt
+echo "data/LC_dewet_prune8_w0.png" | python gridsearch_randomforest.py -k 10 -n 50 -P --seed 1 -D 8 "data/features_dewetting_woclass1.csv"  1> data/pruning8_dewetting_w0.txt
 
 echo "data/LC_dewet_prune2_wo0.png" | python gridsearch_randomforest.py -k 10 -n 50 -P --seed 1 -D 2 "data/features_dewetting_woclass0,1.csv"  1> data/pruning2_dewetting_wo0.txt
 echo "data/LC_dewet_prune3_wo0.png" | python gridsearch_randomforest.py -k 10 -n 50 -P --seed 1 -D 3 "data/features_dewetting_woclass0,1.csv"  1> data/pruning3_dewetting_wo0.txt
